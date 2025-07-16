1c5e5a9adea8b90d760a06dc055d9252c97c4cac
Patrik Pancisin <patrik.pancisin.extern@rwe.com>
2025-07-16 14:10:26 +0200

    chore: remove .gitignore in .idea folder

--- timelapse.py
+++ timelapse.py
@@ -9,6 +9,7 @@ logger = logging_custom.get_logger(__name__)
 root_folder = "."
 file_name = "output"
 folder_filter = ""
+fps = 18
 
 if sys.argv:
     root_folder = sys.argv[1]
@@ -19,6 +20,8 @@ for idx in range(len(sys.argv)):
         file_name=sys.argv[idx + 1]
     if arg.startswith("--filter"):
         folder_filter=sys.argv[idx + 1]
+    if arg.startswith("--fps"):
+        fps=sys.argv[idx + 1]
 
 loader = ImageLoader(root_folder)
 image_files = loader.load(folder_filter)
@@ -26,7 +29,7 @@ image_files = loader.load(folder_filter)
 logger.info(f"Generating timelapse video of {len(image_files)} images. FPS=18")
 
 if len(image_files) > 0:
-    clip = moviepy.video.io.ImageSequenceClip.ImageSequenceClip(image_files, fps=18)
+    clip = moviepy.video.io.ImageSequenceClip.ImageSequenceClip(image_files, fps=fps)
     clip.write_videofile(f"{file_name}.mp4")
 else:
     logger.error("No images found!")
